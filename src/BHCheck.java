5b3522119ecb0585856770c98cb89d8e3626d03c
Emmanuel Olowe <eo32@st-andrews.ac.uk>
2021-10-06 00:59:17 +0100

    printing removed

--- src/BHCheck.java
+++ src/BHCheck.java
@@ -23,12 +23,10 @@ public class BHCheck {
         for (int i = 0; i < this.pSolution.size(); i+=2) {
             int j = i + 1;
             if (this.game.cardAt(this.pSolution.get(i), this.pilesRemaining.get(this.pSolution.get(i)) - 1) != this.pSolution.get(j)) {
-                System.out.println("here");
                 System.out.println(this.game.cardAt(this.pSolution.get(i), this.pilesRemaining.get(this.pSolution.get(i))-1));
                 return false;
             }
             else if (!this.game.adjacent(this.pSolution.get(j), this.holeCard)) {
-                System.out.println("offt");
                 return false;
             }
             this.cardRemaining--;
